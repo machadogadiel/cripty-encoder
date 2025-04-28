51424e4259c588450b9ba0c38f08bb246dcc191d
Gadiel Machado <85411005+machadogadiel@users.noreply.github.com>
2025-04-28 11:43:18 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ function initListeners() {
         event.preventDefault();
 
         const decodedText = decodedTextElement.value
-        const shift = parseInt(document.getElementById("caesar-shift").value)
+        const shift = parseInt(document.getElementById("caesar-shift")?.value)
         
         if (typeSelectElement.selectedIndex == 0) {
             encodedTextElement.value = isEncode() ? btoa(decodedText) : atob(decodedText) 
